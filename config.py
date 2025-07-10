a81719269617307e6c546b39db97840c065dccf5
sarthakbachhar <sarthakbachhar7@gmail.com>
2025-07-10 10:26:03 +0545

    update file contents

--- config.py
+++ config.py
@@ -1,12 +1,8 @@
 import os
-from dotenv import load_dotenv
 
-load_dotenv()
+uri = os.getenv("DATABASE_URL", "sqlite:///inventory.db")
+if uri.startswith("postgres://"):
+    uri = uri.replace("postgres://", "postgresql+psycopg://", 1)
 
-class Config:
-    SECRET_KEY = os.environ.get('SECRET_KEY')
-    uri = os.environ.get('DATABASE_URL', 'sqlite:///inventory.db')
-    if uri.startswith("postgres://"):
-    	uri = uri.replace("postgres://", "postgresql+psycopg://", 1)
-    SQLALCHEMY_DATABASE_URI = uri
-    SQLALCHEMY_TRACK_MODIFICATIONS = False
+SQLALCHEMY_DATABASE_URI = uri
+SQLALCHEMY_TRACK_MODIFICATIONS = False
